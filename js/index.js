87e3928c86be99577b312b1e47be56d719be97fa
unknown <lazycode686@gmail.com>
2022-12-06 01:04:23 -0800

    version1.1.0

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
-// for the nav toggle
-const openNav = () => {
-  const menuWidth = document.getElementById('myNav') 
-  menuWidth.classList.toggle('menu_width')
-
-  //toggle the X from the Hamburger 
-  const menuBtnStyle = document.querySelector('.custom_menu-btn'); 
-  menuBtnStyle.classList.toggle('menu_btn-style')
+// function to add an active class to li onlcick and remove from other li
+function addActiveClass() {
+    var li = document.getElementsByTagName("li");
+    for (var i = 0; i < li.length; i++) {
+        li[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+        });
+    }
 }
+addActiveClass()
+// function to open the navbar on click
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+// for the hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const mobileListedItems = document.querySelector(".mobile-listed-items-container");
+
+
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("is-active");
+    mobileListedItems.classList.toggle('toggle-nav')
+});
