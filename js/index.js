c1965e5879258c327041238c4aed5cc47d7d8ec1
unknown <lazycode686@gmail.com>
2022-12-07 09:37:54 -0800

    version 1.2.0

--- js/index.js
+++ js/index.js
@@ -16,15 +16,51 @@ addActiveClass()
 // for the hamburger menu
 const hamburger = document.querySelector(".hamburger");
 const mobileListedItems = document.querySelector(".mobile-listed-items-container");
-
-
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
     mobileListedItems.classList.toggle('toggle-nav')
 });
 
+
+
+
+
+// ------------ for the nodal
+const modalSignInForm = document.querySelector('#sign-in')
+const usernameField = document.getElementById('usernmae')
+function signIn() {
+  modalSignInForm.style.display = 'flex'
+  document.getElementById('usernmae').focus()
+  // console.log(usernameField);
+}
+function closeModal() {
+  modalSignInForm.style.display = 'none'
+}
+
+// mobile sign up modal
+function signIn2() {
+  modalSignInForm.style.display = 'flex'
+  usernameField.focus()
+
+  // when i click the sign up toggle back the HAMBURGER MENU AND THE NAV
+  hamburger.classList.toggle("is-active");
+  mobileListedItems.classList.toggle('toggle-nav')
+}
+
+// --------- end for the modal
+
+//scroll reveal js implementation
+ScrollReveal().reveal('.scroll-reveal', {
+  interval: 16,
+  reset: true
+});
+
+
+ScrollReveal().reveal('.mll1', { delay: 1000 });
+ScrollReveal().reveal('.btn', { delay: 3000 });
+
+
 // function for the "you dont hve to loose one" text animation
-// Wrap every letter in a span
 var textWrapper = document.querySelector('.ml10 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -40,12 +76,4 @@ anime.timeline({loop: true})
     duration: 1000,
     easing: "easeOutExpo",
     delay: 1000
-  });
-
-  ScrollReveal().reveal('.scroll-reveal', {
-    interval: 16,
-    reset: true
-  });
-
-  ScrollReveal().reveal('.mll1', { delay: 1000 });
-  ScrollReveal().reveal('.btn', { delay: 3000 });
+});
