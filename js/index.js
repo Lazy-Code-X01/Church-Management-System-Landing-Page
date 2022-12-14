2258bda6140fc82f4b6af70ead1c4414a96e3717
unknown <lazycode686@gmail.com>
2022-12-13 17:32:25 -0800

    version 1.7.0

--- js/index.js
+++ js/index.js
@@ -123,20 +123,24 @@ scrollBtn.addEventListener("click", () => {
 // end
 
 
-// function for the "you dont hve to loose one" text animation
-var textWrapper = document.querySelector('.ml10 .letters');
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml12');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml10 .letter',
-    rotateY: [-90, 0],
-    duration: 1300,
-    delay: (el, i) => 45 * i
-  }).add({
-    targets: '.ml10',
-    opacity: 0,
-    duration: 1000,
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
     easing: "easeOutExpo",
-    delay: 1000
-});
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
